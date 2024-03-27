8139b20627a96dc6616aa4ba413f3e61768035ac
jonabitbol <tbnrsamurai@gmail.com>
2024-03-27 14:07:09 -0400

    added script for fetching musicbrainz id based on title/artist from spotify

---
+++ Scripts/music_brainz.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_music_brainz_id(recording_name, artist_name):
+    query = f'artist:{artist_name}%20AND%20recording:{recording_name}'
+    response = requests.get(f'https://musicbrainz.org/ws/2/recording?query={query}&fmt=json')
+    response.raise_for_status()
+    data = response.json()
+    if data['count'] == 0:
+        return None
+    return data['recordings'][0]['id']
+
+with(open('spotify_data.csv', 'r')) as file:
+    for line in file:
+        song_id, song_name, artist_name = line.strip().split(',')
+        music_brainz_id = get_music_brainz_id(song_name, artist_name)
+        if music_brainz_id:
+            print(f"Song: {song_name}, Artist: {artist_name}, MusicBrainz ID: {music_brainz_id}")
+        else:
+            print(f"Song: {song_name}, Artist: {artist_name}, MusicBrainz ID not found")
--- Scripts/spotify.py
+++ Scripts/spotify.py
@@ -1,13 +1,17 @@
 # This is a script to populate a database with Spotify data
 
 import spotipy
+import os
+from dotenv import load_dotenv
 
 from spotipy import SpotifyClientCredentials
 
 # Set up the Spotify API
-# client_id = os.environ['SPOTIFY_CLIENT_ID']
-client_id = ''
-client_secret = ''
+load_dotenv()
+client_id = os.environ['SPOTIFY_CLIENT_ID']
+client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+# client_id = '461fba45b2a24aff81b6cbcd3f24266e'
+# client_secret = 'c5ea6c9c70434e9da20ccfa4a7e12482'
 
 sp = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
@@ -22,21 +26,26 @@ def get_playlist_tracks(uri):
         results = sp.next(results)
         tracks.extend(results['items'])
 
-    # Extracting only the desired information: song ID, song name, and artist(s)
+    # Extracting only the desired information: song ID, song name, and artist(s) but only if there's exactly one artist
     simplified_tracks = []
     for item in tracks:
         track = item['track']
-        track_info = {
-            'song_id': track['id'],
-            'song_name': track['name'],
-            'artists': [artist['name'] for artist in track['artists']]
-        }
-        simplified_tracks.append(track_info)
+        if len(track['artists']) == 1:  # Check if there's exactly one artist
+            track_info = {
+                'song_id': track['id'],
+                'song_name': track['name'],
+                'artists': [artist['name'] for artist in track['artists']]
+            }
+            simplified_tracks.append(track_info)
 
     return simplified_tracks
 
 
-playlist_uri = ''
+
+playlist_uri = 'https://open.spotify.com/playlist/37i9dQZF1DWY4xHQp97fN6'
 for track in get_playlist_tracks(playlist_uri):
     print(f"ID: {track['song_id']}, Song: {track['song_name']}, Artists: {', '.join(track['artists'])}")
 
+with open('spotify_data.csv', 'w') as file:
+    for track in get_playlist_tracks(playlist_uri):
+        file.write(f"{track['song_id']},{track['song_name']},{', '.join(track['artists'])}\n")
